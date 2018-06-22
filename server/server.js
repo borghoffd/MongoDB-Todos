6902cba9acb6a2dbb38f3d044634312ca49b504f
Dennis Borghoff <borghoffd@gmail.com>
2018-06-22 11:19:52 +0200

    Added /todos/:id route and testcases

--- server/server.js
+++ server/server.js
@@ -33,18 +33,16 @@ app.get("/todos",(req, res) => {
 app.get("/todos/:id", (req,res) => {
     let id = req.params["id"];
     if(!ObjectID.isValid(id)) {
-        res.send("ID is not valid!");
-    } else {
-        Todo.findById(id).then((todo) => {
-            if(!todo) {
-                res.send("ID not found!");
-            } else {
-                res.send(todo);
-            }
-        }).catch((e) => {
-            res.send(e);
-        });
+        return res.status(404).send();
     }
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
